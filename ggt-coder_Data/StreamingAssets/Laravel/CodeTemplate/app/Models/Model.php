263c748073171c53d88eea6ebcf9f6524b140ea8
侯翼宁 - CuratorC <mail@curatorc.com>
2021-03-09 17:24:20 +0800

    init project

---
+++ ggt-coder_Data/StreamingAssets/Laravel/CodeTemplate/app/Models/Model.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Traits\MiddleTableOperate;
+use App\Models\Traits\ScopeCoderSearch;
+use Illuminate\Database\Eloquent\Model as EloquentModel;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Model extends EloquentModel
+{
+    use HasFactory;
+    use SoftDeletes; // 软删除
+    use ScopeCoderSearch; // scope 查询
+    use MiddleTableOperate; // 中间表操作
+}
+
